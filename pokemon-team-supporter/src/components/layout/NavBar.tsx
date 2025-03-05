d9839bb72b85c720675ee971d45e10ffbfaab4e5
Ben-cyber-hub <71332957+Ben-cyber-hub@users.noreply.github.com>
2025-03-05 11:41:19 +0000

    Remove sensitive environment files and update NavBar color; add Team ID display in TeamDetails

--- pokemon-team-supporter/src/components/layout/NavBar.tsx
+++ pokemon-team-supporter/src/components/layout/NavBar.tsx
@@ -3,7 +3,7 @@ import pokeball from '../../assets/images/pokeball-icon.png';
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-800 shadow-lg">
+    <nav className="bg-orange-600 shadow-lg">
       <div className="max-w-7xl mx-auto px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
